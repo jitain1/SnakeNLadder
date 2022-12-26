62c0f3c7c022505c1401f1f0d6a28e027d549230
jitain1 <singhjitainj@gmail.com>
2022-12-26 16:49:31 +0530

    Game Continues untill Winning Added

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -1,46 +1,55 @@
 package com.bridgelabz;
 
 public class SnakeNLadder {
-	
 	static final int START_POSITION = 0;
+	static final int FINAL_POSITION = 100;
 	
 	static final int NO_PLAY = 0;
 	static final int IS_SNAKE = 1;
 	static final int IS_LADDER = 2;
 	
+	static int diceCount =0;
+	
 	static int diceRoll() {
 		int x = (int) (1+ Math.random() * 6);
 		return x;
 	}
-	
 	static int getOption() {
 		int x = (int) (Math.random() * 3);
 		return x;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println("Welcome To Snake N Ladder Game");
+		System.out.println("Welcome to snake and ladder problem.");
 		
 		int playerPosition = START_POSITION;
-		
-		int roll = diceRoll();
-		System.out.println(roll);
-		
-		int option = getOption();
-		switch (option) {
-		case IS_SNAKE :
-			System.out.println("Snake");
-			playerPosition -= roll;
-			break;
-		case IS_LADDER :
-			System.out.println("Ladder");
-			playerPosition += roll;
-			break;
-		default:
-			System.out.println("No play");
-
+	
+		while (playerPosition <= FINAL_POSITION) {
+			diceCount++;
+			
+			int roll = diceRoll();
+			System.out.println(roll);
+			
+			int option = getOption();
+			switch (option) {
+			case IS_SNAKE :
+				System.out.println("Snake");
+				playerPosition -= roll;
+				if(playerPosition < START_POSITION) 
+					playerPosition = START_POSITION;
+				System.out.println("Player position : " + playerPosition);
+				break;
+			case IS_LADDER :
+				System.out.println("Ladder");
+				playerPosition += roll;
+				System.out.println("Player position : " + playerPosition);
+				break;
+			default:
+				System.out.println("No play");
+				System.out.println("Player position : " + playerPosition);
+			}	
 		}
-		
-		System.out.println("Player position : " + playerPosition);
+		System.out.print("Total Dice count : " + diceCount);
+		System.out.println("Finally Player position : " + playerPosition);	
 	}
 }
