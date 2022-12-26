840c045f8cd91cea95d5ab64eac3e710340ccda1
jitain1 <singhjitainj@gmail.com>
2022-12-26 15:58:01 +0530

    Initial Position Added

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -2,7 +2,9 @@ package com.bridgelabz;
 
 public class SnakeNLadder {
 	
+	static final int START_POSITION = 0;
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome To Snake AND Ladder PROGRAM	");
+		System.out.println("Welcome To Snake N Ladder Game");
 	}
 }
