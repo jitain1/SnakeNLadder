a721d98403ccef4931233fa2cc7d346807b6e87c
jitain1 <singhjitainj@gmail.com>
2022-12-26 16:28:14 +0530

    Player Playing Cases Added

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -4,15 +4,43 @@ public class SnakeNLadder {
 	
 	static final int START_POSITION = 0;
 	
+	static final int NO_PLAY = 0;
+	static final int IS_SNAKE = 1;
+	static final int IS_LADDER = 2;
+	
 	static int diceRoll() {
 		int x = (int) (1+ Math.random() * 6);
+		return x;
+	}
+	
+	static int getOption() {
+		int x = (int) (Math.random() * 3);
 		return x;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake N Ladder Game");
 		
+		int playerPosition = START_POSITION;
+		
 		int roll = diceRoll();
 		System.out.println(roll);
+		
+		int option = getOption();
+		switch (option) {
+		case IS_SNAKE :
+			System.out.println("Snake");
+			playerPosition -= roll;
+			break;
+		case IS_LADDER :
+			System.out.println("Ladder");
+			playerPosition += roll;
+			break;
+		default:
+			System.out.println("No play");
+
+		}
+		
+		System.out.println("Player position : " + playerPosition);
 	}
 }
